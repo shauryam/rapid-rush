3ed6ae80fe5c2d22d897a542b4db41abbdeb9707
Shaurya Mittal <shaurya_mittal@ymail.com>
2025-02-21 16:31:16 -0800

    Initial Tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,43 +1,36 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import {
+  AntDesign,
+  Feather,
+  Ionicons,
+  MaterialIcons,
+} from "@expo/vector-icons";
+import { Tabs } from "expo-router";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
+    <Tabs>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Play",
+          headerShown: false,
+          tabBarIcon: () => <Feather name="play" />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="settings"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Settings",
+          headerShown: false,
+          tabBarIcon: () => <Ionicons name="settings-outline" />,
+        }}
+      />
+      <Tabs.Screen
+        name="instructions"
+        options={{
+          title: "Game Instructions",
+          headerShown: false,
+          tabBarIcon: () => <MaterialIcons name="format-list-bulleted" />,
         }}
       />
     </Tabs>
---
+++ app/(tabs)/instructions.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function Instructions() {
+  return <Text>Instructions</Text>;
+}
